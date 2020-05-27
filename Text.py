64f78e8a9ec1b57b7e061223f85618d00b1f95d7
ENG Ahmed Mahmoud <ahmedsr12@yahoo.com>
2020-05-27 11:14:09 +0200

    maintenance +++

--- Text.py
+++ Text.py
@@ -9,25 +9,25 @@ class text:
         self.finalShape = self.originalShape
         self.errorvisualized = self.originalShape
         if '<' in self.originalShape:
-            self.errorvisualized = self.errorvisualized.replace('<', '(ERR:<)')
+            self.errorvisualized = self.errorvisualized.replace('<', '(ERR:Using <)')
             self.finalShape = self.finalShape.replace('<', '&lt;')
             self.errors+=1
         if '>' in self.originalShape:
-            self.errorvisualized = self.errorvisualized.replace('>', '(ERR:>)')
+            self.errorvisualized = self.errorvisualized.replace('>', '(ERR:Using >)')
             self.finalShape = self.finalShape.replace('>', '&gt;')
             self.errors += 1
         if '&' in self.originalShape:
-            self.errorvisualized = self.errorvisualized.replace('&', '(ERR:&)')
-            self.errorvisualized = self.errorvisualized.replace('(ERR:&)' + 'amp;', '&amp;')
+            self.errorvisualized = self.errorvisualized.replace('&', '(ERR:Using &)')
+            self.errorvisualized = self.errorvisualized.replace('(ERR:Using &)' + 'amp;', '&amp;')
             self.finalShape = self.finalShape.replace('&', '&amp;')
             self.finalShape = self.finalShape.replace('&amp;amp;', '&amp;')
             self.errors += 1
         if '\'' in self.originalShape:
-            self.errorvisualized = self.errorvisualized.replace('\'', "(ERR:\')")
+            self.errorvisualized = self.errorvisualized.replace('\'', "(ERR:Using \')")
             self.finalShape = self.finalShape.replace('\'', '&apos;')
             self.errors += 1
         if '\"' in self.originalShape:
-            self.errorvisualized = self.errorvisualized.replace('\"', "(ERR:\")")
+            self.errorvisualized = self.errorvisualized.replace('\"', "(ERR:Using \")")
             self.finalShape = self.finalShape.replace('\"', '&quot;')
             self.errors += 1
         return self.errors
